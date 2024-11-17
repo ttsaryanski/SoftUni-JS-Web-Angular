7cac3fcbc0d56d5bafe5211f139cb9059880c762
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-11-17 18:23:21 +0200

    Add isAuthenticated guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,26 +8,38 @@ import { CreateThemeComponent } from './theme/create-theme/create-theme.componen
 import { MainComponent } from './main/main.component';
 import { DetailsThemeComponent } from './theme/details-theme/details-theme.component';
 import { PostsComponent } from './posts/posts.component';
+import { isAuthenticated } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
 
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [isAuthenticated],
+    },
 
-  {
-    path: 'themes',
-    children: [
-      { path: '', component: MainComponent },
-      { path: ':themeId', component: DetailsThemeComponent },
-    ],
-  },
-  { path: 'create-theme', component: CreateThemeComponent },
+    {
+        path: 'themes',
+        children: [
+            { path: '', component: MainComponent },
+            {
+                path: ':themeId',
+                component: DetailsThemeComponent,
+            },
+        ],
+    },
+    {
+        path: 'create-theme',
+        component: CreateThemeComponent,
+        canActivate: [isAuthenticated],
+    },
 
-  { path: 'posts', component: PostsComponent },
+    { path: 'posts', component: PostsComponent },
 
-  { path: '404', component: ErrorComponent },
-  { path: '**', redirectTo: '/404' },
+    { path: '404', component: ErrorComponent },
+    { path: '**', redirectTo: '/404' },
 ];
